522dace6aca09e305f84399d8c2cd2fba49076db
erezrokah <erezrokah@users.noreply.github.com>
2020-03-21 22:49:56 +0200

    chore: fix semantic release

--- release.config.js
+++ release.config.js
@@ -1,12 +1,9 @@
 module.exports = {
   plugins: [
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json'],
-        message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git',
   ],
 };
