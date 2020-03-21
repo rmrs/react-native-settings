2cd720fe1b2454be18eb1503c22d28d6b728b271
Erez Rokah <erezrokah@users.noreply.github.com>
2020-03-21 19:57:49 +0200

    chore(deps): update example (#53)

--- example/__tests__/App-test.js
+++ example/__tests__/App-test.js
@@ -4,9 +4,10 @@
 
 import 'react-native';
 import React from 'react';
+import App from '../App';
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import App from '../App';
 
 jest.mock('react-native-settings', () => {
   return { getSetting: jest.fn(() => Promise.resolve()) };
